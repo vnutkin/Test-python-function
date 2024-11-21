7a721ef5e88044076cd1a5eaec1d32caf366343a
vnutkin <vl_utkin@yahoo.com>
2024-11-21 14:37:13 +0300

    test1

--- main.py
+++ main.py
@@ -2,14 +2,15 @@ def fun(n):
     a = n
     b = n ** 2
     c = n ** 3
-    return a, b, c
+    d = n ** 4
+    return a, b, c, d
 
 
-y1, y2, y3 = fun(2)
-print(y1, y2, y3)
-c, b, a = fun(2)
-print(a, b, c)
-y1, y2, b = fun(2)
-print(y1, y2, b)
-b, y1, y2 = fun(2)
-print(y1,y2,b)
+y1, y2, y3, y4 = fun(2) #1
+print(y1, y2, y3, y4)
+d, c, b, a = fun(2)
+print(a, b, c, d)   #2
+y1, d, y2, c = fun(2)
+print(y1, y2, c, d)  #3
+b, y1, y2, c = fun(2)
+print(y1, y2, b, c)  #4
